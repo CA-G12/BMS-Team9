7fa80c8481228cdaeb0e324c974ae45feba63795
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-24 16:36:28 +0300

    Added the helmet config object and used some middlewares close #27

--- src/app.js
+++ src/app.js
@@ -5,28 +5,18 @@ const compression = require('compression');
 const helmet = require('helmet');
 
 const router = require('./router');
-const {
-  getDataPage,
-  getGenerals,
-  databaseSearch,
-  insertData,
-} = require('./controllers/index');
-const {
-  serverError,
-  notFound,
-} = require('./controllers/errorHandling/index');
 
 const app = express();
 
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(compression);
-app.use(helmet());
+app.use(helmet.contentSecurityPolicy({ useDefaults: true, directives: { 'img-src': ["'self'", 'https: data:'] } });
 
 app.use(express.static(join(__dirname, '..', 'public')));
 
-app.use(router());
+app.use(router);
 
-module.exports = { app };
+module.exports = app;
