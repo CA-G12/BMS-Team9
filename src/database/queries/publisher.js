27e93d3045d800f64579f50bbfa171d5680dfd80
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-25 09:58:08 +0300

    Added the databases queries

--- src/database/queries/publisher.js
+++ src/database/queries/publisher.js
@@ -1,6 +1,29 @@
 const connection = require('../config/connection');
 
-// eslint-disable-next-line no-unused-vars
-const publisherData = (req, res) => connection.query('Select name, address, contact_num from Publishers');
+class Publisher {
+  constructor() {
+    this.getDataQuery = 'Select name, address, contact_num from Publishers';
+    this.insertQuery = {
+      text: 'insert into Publishers(name, address, contact_num, book_id) values ($1, $2, $3, $4) returning *',
+      values: [],
+    };
+  }
 
-module.exports = { publisherData };
+  getGeneralData() {
+    return connection.query(this.getDataQuery);
+  }
+
+  insertData(req) {
+    const {
+      // eslint-disable-next-line camelcase
+      pub_name, address, contact, book_name,
+    } = req.body;
+    // eslint-disable-next-line camelcase
+    this.insertQuery.values = [pub_name, address, Number(contact), Number(book_name)];
+    return connection.query(this.insertQuery);
+  }
+}
+
+const publisher = new Publisher();
+
+module.exports = publisher;
